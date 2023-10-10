7e5e873a927d4c589f57653c105a6d6da3cf901b
sabdulb <rand192763@gmail.com>
2023-10-10 14:56:07 -0400

    added hard coded name-based urls

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+def brian(request):
+    return HttpResponse("Hello, brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
