3c955c3ffa3b867696e8a2ea9771ebf9c1a3d9e0
sabdulb <rand192763@gmail.com>
2023-10-10 14:59:08 -0400

    added dynamic name routes to hello

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("brian", views.brian, name="brian"),
-    path("david", views.david, name="david")
+    path("david", views.david, name="david"),
+    path("<str:name>", views.greet, name="greet")
 ]
